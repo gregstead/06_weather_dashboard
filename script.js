15b2498d8b73ad4778bf5c14552cb57d127dfdb7
Greg Stead <gregstead@gmail.com>
2020-10-18 21:51:45 -0500

    Rough placement of Materialize elements look functional on mobile size

--- script.js
+++ script.js
@@ -16,10 +16,10 @@ $(document).on('click', function (event) {
         // Query params
         // Get city name from text area
         // TODO: Authenticate city input
-        var $cityInput = 'q=' + $('#city-input').val();
+        var $cityInput = 'q=' + checkedCity($('#city-input').val());
         var API_Key = '&appid=' + 'f4330d7ea944cf18c17c360fd45ea7dd';
         var units = '&units=imperial'
-        // Get current weather JSON for city from openweathermap API
+        // Get current weather JSON for city from openweathermap 5 day forecast API
 
         var queryURL = "https://api.openweathermap.org/data/2.5/forecast?" + $cityInput + API_Key + units;
 
@@ -28,9 +28,8 @@ $(document).on('click', function (event) {
             url: queryURL,
             method: "GET",
         }).then(function (res) {
-            // <!-- City, temp, humidity, wind speed, UV index, weather icon -->
 
-            response = res;
+            // response = res;
             // Render page elements from response
             renderCurrentWeather(res);
             // Render 5 day forecast 
@@ -44,36 +43,66 @@ $(document).on('click', function (event) {
     }
 })
 
+// String => String
+// Checks city name input against valid city ID
+function checkedCity(cityNameString) {
+    // Compare city name against list of checked names
+    return cityNameString;
+};
+
 // JSON => HTML
 // Takes JSON response from openweather and renders current weather HTML
 function renderCurrentWeather(response) {
+    // Empty div
+    $currentWeatherEl.empty();
 
-    $currentWeatherEl.append("<p>" + response.city.name + "</p>");
+    var fDate = moment(response.list[0].dt_txt).format(' (MM/DD/YYYY)');
+    $currentWeatherEl.append("<h5>" + response.city.name + fDate + "</h5>");
     // TODO get date and icon
-    $currentWeatherEl.append("<p>" + response.list[0].main.temp + "</p>");
-    $currentWeatherEl.append("<p>" + response.list[0].main.humidity + "</p>");
-    $currentWeatherEl.append("<p>" + response.list[0].wind.speed + "</p>");
+    
+    $currentWeatherEl.append("<p>" + 'Temperature: ' + response.list[0].main.temp + "</p>");
+    $currentWeatherEl.append("<p>" + 'Humidity: ' + response.list[0].main.humidity + "</p>");
+    $currentWeatherEl.append("<p>" + 'Wind speed: ' + response.list[0].wind.speed + "</p>");
 };
 
+// JSON => HTML
+// Takes JSON response from openweather and renders five day weather HTML
 function renderFiveDayForecase(response){
-    for (i = 0; i < 5; i++) {
-        // Card containing Date, icon, temp, humidity
+    // Empty div
+    $fiveDayWeatherEl.empty();
+
+    for (i = 1; i < 40; i += 8) {
+        
+        // Make DOM node for card
+
+        $forecastCardContainer = $('<div>');
+        $forecastCardContainer.attr('class','col s6 m3 l2')
+
         $forecastCard = $('<div>');
+        $forecastCard.attr('class','card blue-grey hoverable');
+        //  Card title - Date MM/DD/YYYY
+        $forecastCardTitle = $('<h7>');
+        var fDate = moment(response.list[i].dt_txt).format('MM/DD/YYYY');
+        $forecastCardTitle.attr('class','white-text left-align').text(fDate);
+        // Card content
+        $forecastCardContent = $('<div>');
+        $forecastCardContent.attr('class','card-content white-text')
 
-        $forecastCard.append("<p>" + response.list[i+1].dt + "</p>");
-        $forecastCard.append("<p>" + response.list[i+1].main.temp + "</p>");
-        $forecastCard.append("<p>" + response.list[i+1].main.humidity + "</p>");
+        $forecastCardContent.append("<p>" + 'Temp: ' + response.list[i+1].main.temp + "</p>");
+        $forecastCardContent.append("<p>" + 'Humidity: ' + response.list[i+1].main.humidity + "</p>");
 
-        $fiveDayWeatherEl.append($forecastCard);
+        $forecastCard.append($forecastCardTitle);
+        $forecastCard.append($forecastCardContent);
+
+        $forecastCardContainer.append($forecastCard);
+
+        // Add card node to DOM
+        $fiveDayWeatherEl.append($forecastCardContainer);
 
     };
 };
 
-
-// String => Object
-// Get current weather JSON for city from openweathermap API
-
-
-// Make node elements
-
-// write node elements to page
+// Unix timestamp => Formatted date string
+function unixToDate(timestamp) {
+    console.log('nothing');
+}
